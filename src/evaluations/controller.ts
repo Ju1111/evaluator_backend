054d22cbb1c745fc031732cfd703dccc56fd63c9
Ju111 <SchneiderJu@gmx.net>
2018-04-03 20:21:04 +0200

    add evaluations controller endpoints

---
+++ src/evaluations/controller.ts
@@ -0,0 +1,42 @@
+import { JsonController, Param, BadRequestError, NotFoundError, Get, Body, HttpCode, Post, Authorized, Put } from 'routing-controllers'
+import Evaluation from './entity'
+
+@JsonController()
+export default class EvaluationController {
+
+  @Get('/evaluations')
+  @HttpCode(200)
+  async getEvaluations(
+  ) {
+    return Evaluation.find()
+  }
+
+  @Get('/evaluation/:id([0-9]+)')
+  @HttpCode(200)
+  async getEvaluation(
+    @Param('id') evaluationId: number
+  ) {
+    const evaluation = await Evaluation.findOneById(evaluationId)
+    if (!evaluation) throw new NotFoundError('Evaluation does not exist!')
+    return evaluation
+  }
+
+  @Post('/evaluations')
+  @HttpCode(201)
+  async createEvaluation(
+    @Body() evaluation: Evaluation
+  ) {
+    return evaluation.save();
+  }
+
+  @Put('/evaluations/:id')
+  async updateEvaluation(
+    @Param('id') evaluationId:number,
+    @Body() update: Partial<Evaluation>
+  ) {
+    const evaluation = await Evaluation.findOneById(evaluationId)
+    if (!evaluation) throw new NotFoundError('Evaluation does not exist!')
+
+    return Evaluation.merge(evaluation, update)
+  }
+}
