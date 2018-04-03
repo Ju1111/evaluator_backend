bcfd0f3afcfe11deafdbfcee839cbe6a63eb9e5d
Ju111 <SchneiderJu@gmx.net>
2018-04-03 22:17:35 +0200

    test student controller endpoints

---
+++ src/students/controller.spec.ts
@@ -0,0 +1,60 @@
+import 'jest'
+import * as request from 'supertest'
+import { app } from '../app'
+import setupDb from '../db'
+
+beforeAll(async () => {
+  await setupDb()
+})
+
+describe('StudentController', () => {
+  test('/students', async () => {
+    await request(await app.callback())
+    .get('/students')
+    .set('Accept', 'application/json')
+    .expect(200)
+  })
+
+  test('/students/:id([0-9]+)', async() => {
+    await request(await app.callback())
+    .get('/students/2')
+    .set('Accept', 'application/json')
+    .expect(200)
+  })
+
+  test('/students', async() => {
+
+    const target = {
+      firstName: 'Julia',
+      lastName: 'Schneider',
+      picture: 'https://ca.slack-edge.com/T0DK39WAJ-U93K8PRTL-7f96d583abdc-512'
+    }
+
+    const response = await request(await app.callback())
+    .post('/students')
+    .set('Accept', 'application/json')
+    .send(target)
+    .expect(201)
+  })
+
+  test('/students/:id', async() => {
+
+    const tar = {
+      firstName: 'Ju'
+    }
+
+    const res = await request(await app.callback())
+    .put('/students/1')
+    .set('Accept', 'application/json')
+    .send(tar)
+    .expect(201)
+  })
+
+  test('/students/id', async() => {
+    await request(await app.callback())
+    .delete('/students/2')
+    .set('Accept', 'application/json')
+    .expect(201)
+  })
+
+})
